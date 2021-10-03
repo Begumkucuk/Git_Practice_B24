80a281ba1c8cb5a8d8eb7d2ee7484810b63ef5f2
begum kucuk <begum31194@hotmail.com>
2021-10-03 12:23:57 -0400

    updating second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -6,4 +6,10 @@ public class Second {
         System.out.println("Hello World");
 
     }
-}
+    public void m(int a){
+        System.out.println("First method");
+    }
+    public void m(String s){
+        System.out.println("Second method");
+    }
+        }
